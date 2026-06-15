fdb2b653b342c8ade3083f64cb50b7eae1071e48
Ajaysinghratnawat <ajaysinghratnawat113@email.com>
2026-06-15 11:37:20 +0530

    Merchant and Order Api create

--- razorpay/src/main/java/com/codingshuttle/razorpay/merchant/service/Impl/ApiKeyServiceImpl.java
+++ razorpay/src/main/java/com/codingshuttle/razorpay/merchant/service/Impl/ApiKeyServiceImpl.java
@@ -2,17 +2,23 @@ package com.codingshuttle.razorpay.merchant.service.Impl;
 
 
 import com.codingshuttle.razorpay.common.exception.ResourceNotFoundException;
+import com.codingshuttle.razorpay.common.util.RandomizerUtil;
 import com.codingshuttle.razorpay.merchant.dto.request.CreateApiKeyRequest;
 import com.codingshuttle.razorpay.merchant.dto.response.ApiKeyCreateResponse;
+import com.codingshuttle.razorpay.merchant.dto.response.ApiKeyResponse;
 import com.codingshuttle.razorpay.merchant.entity.ApiKey;
 import com.codingshuttle.razorpay.merchant.entity.Merchant;
 import com.codingshuttle.razorpay.merchant.repository.ApiKeyRepository;
 import com.codingshuttle.razorpay.merchant.repository.MerchantRepository;
 import com.codingshuttle.razorpay.merchant.service.ApiKeyService;
+import jakarta.annotation.Nullable;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -24,14 +30,14 @@ public class ApiKeyServiceImpl implements ApiKeyService {
     private final ApiKeyRepository apiKeyRepository;
 
     @Override
+    @Transactional
     public ApiKeyCreateResponse create(UUID merchantId, CreateApiKeyRequest request) {
         Merchant merchant = merchantRepository.findById(merchantId)
                 .orElseThrow(() -> new ResourceNotFoundException("merchant", merchantId));
 
-        String keyId = "rzp_"+request.environment().name().toUpperCase()+"big_random_string";
-        String rawSecret = "big_random_secret"; // TODO: replace with cryptographic random hex
-//        a-z,A-Z,0-9,-,_
-//        a-z,0-9
+        String keyId = "rzp_"+request.environment().name().toLowerCase()+"_"+ RandomizerUtil.randomBase64(24);
+        String rawSecret = RandomizerUtil.randomBase64(40);
+
 
         ApiKey apiKey = ApiKey.builder()
                 .merchant(merchant)
@@ -43,5 +49,46 @@ public class ApiKeyServiceImpl implements ApiKeyService {
         apiKey = apiKeyRepository.save(apiKey);
 
         return new ApiKeyCreateResponse(apiKey.getId(), keyId, rawSecret, request.environment());
+    }
+
+    @Override
+    public List<ApiKeyResponse> listByMerchant(UUID merchantId) {
+        return apiKeyRepository.findByMerchant_Id(merchantId).stream()
+                .map(apiKey ->
+                        new ApiKeyResponse(
+                                apiKey.getId(),
+                                apiKey.getKeyId(),
+                                apiKey.getEnvironment(),
+                                apiKey.isEnabled(),
+                                apiKey.getLastUsedAt(), null))
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public void revoke(UUID merchantId, UUID keyId) {
+        ApiKey key = apiKeyRepository.findById(keyId)
+                .filter(k -> k.getMerchant().getId().equals(merchantId))
+                .orElseThrow(() -> new ResourceNotFoundException("ApiKey", keyId));
+
+        key.setEnabled(false);
+    }
+
+    @Override
+    @Transactional
+    public @Nullable ApiKeyCreateResponse rotate(UUID merchantId, UUID keyId) {
+        ApiKey apiKey = apiKeyRepository.findById(keyId)
+                .filter(k -> k.getMerchant().getId().equals(merchantId))
+                .orElseThrow(() -> new ResourceNotFoundException("ApiKey", keyId));
+
+        String newRawSecret = RandomizerUtil.randomBase64(40);
+        apiKey.setPreviousKeySecretHash(apiKey.getKeySecretHash());
+        apiKey.setKeySecretHash(newRawSecret);  // TODO: encode with BcryptPasswordEncoder
+        apiKey.setRotatedAt(LocalDateTime.now());
+        apiKey.setGracePeriodExpiresAt(LocalDateTime.now().plusHours(24));
+        apiKey = apiKeyRepository.save(apiKey);
+
+        return new ApiKeyCreateResponse(apiKey.getId(), apiKey.getKeyId(),
+                newRawSecret, apiKey.getEnvironment());
     }
 }
